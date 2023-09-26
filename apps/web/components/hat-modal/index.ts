539b951c300b080bedc3131583f93c6d14a5550c
Knevari <winresource000@gmail.com>
2023-09-26 19:52:14 -0300

    fix: adjust exports

--- apps/web/components/hat-modal/index.ts
+++ apps/web/components/hat-modal/index.ts
@@ -1,1 +1,5 @@
+export { HatColor } from "./hat-color";
+export { HatDetails } from "./hat-details";
+export { HatEdit } from "./hat-edit";
 export { HatModal } from "./hat-modal";
+export { HatSize } from "./hat-size";
