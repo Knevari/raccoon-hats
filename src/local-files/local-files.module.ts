0a109930a787b13a11bb6fbe882c9cb473e4628f
Knevari <winresource000@gmail.com>
2023-09-28 15:10:55 -0300

    Delete src/local-files directory

--- src/local-files/local-files.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LocalFilesService } from './local-files.service';
-
-@Module({
-  providers: [LocalFilesService]
-})
-export class LocalFilesModule {}
